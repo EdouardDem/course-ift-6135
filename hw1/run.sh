bb0c95e046c91c96d7b9ad0c049c06079ce91ea6
Edouard Demotes <e.demotes@gmail.com>
2025-02-20 22:54:48 -0500

    faet: runner

---
+++ hw1/run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Run all experiments
+./question-2.sh
+./question-3.sh
+./question-4.sh
+./question-5.sh
+./question-6.sh
+./question-7.sh
+
+# Run all plots
+python question-2-plot.py
+python question-3-plot.py
+python question-4-plot.py
+python question-5-plot.py
+python question-6-plot.py
+python question-7-plot.py
