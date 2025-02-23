2ea2696684e3dea4736ce1b229d5d02a5620306e
Edouard Demotes <e.demotes@gmail.com>
2025-02-23 18:50:01 -0500

    chore: add 7-bis in runner

--- hw1/run.sh
+++ hw1/run.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
 # Run all experiments
-./question-2.sh
-./question-3.sh
-./question-4.sh
-./question-5.sh
-./question-6.sh
-./question-7.sh
+# ./question-2.sh
+# ./question-3.sh
+# ./question-4.sh
+# ./question-5.sh
+# ./question-6.sh
+# ./question-7.sh
+./question-7-bis.sh
 
 # Run all plots
-python question-2-plot.py
-python question-3-plot.py
-python question-4-plot.py
-python question-5-plot.py
-python question-6-plot.py
-python question-7-plot.py
+# python question-2-plot.py
+# python question-3-plot.py
+# python question-4-plot.py
+# python question-5-plot.py
+# python question-6-plot.py
+# python question-7-plot.py
+python question-7-bis-plot.py
