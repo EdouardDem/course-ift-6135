f6455c359e1f97dec9acf1b6f08e0cd8b0df7535
Edouard Demotes <e.demotes@gmail.com>
2025-03-06 13:37:14 -0500

    chore: rerun question 4

--- hw1/question-4.sh
+++ hw1/question-4.sh
@@ -3,7 +3,7 @@
 set -e
 
 function run_experiment() {
-    python main.py --model mlpmixer --model_config ./model_configs/question-4/$1.json --visualize --epochs 40 --logdir ./logs/question-4/$1
+    python main.py --model mlpmixer --model_config ./model_configs/question-4/$1.json --visualize --epochs 30 --logdir ./logs/question-4/$1
 }
 
 run_experiment mlpmixer-2
