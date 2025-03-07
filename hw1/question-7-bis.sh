a4e762ffb5ce2c5ad835defb3c7d0ce1ecbde335
Edouard Demotes <e.demotes@gmail.com>
2025-03-07 07:11:19 -0500

    feat: run qestion 7 bis with fix

--- hw1/question-7-bis.sh
+++ hw1/question-7-bis.sh
@@ -3,7 +3,7 @@
 set -e
 
 function run_experiment() {
-    python main.py --model mlpmixer --model_config ./model_configs/question-7-bis/$1.json --visualize --epochs 60 --logdir ./logs/question-7-bis/$1
+    python main.py --model mlpmixer --model_config ./model_configs/question-7-bis/$1.json --visualize --epochs 100 --logdir ./logs/question-7-bis/$1
 }
 
 run_experiment mlpmixer-e128
