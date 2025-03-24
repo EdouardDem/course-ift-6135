38b4ffe28468c8e77d6f90bd4addd4e5b2e5066e
Edouard Demotes <e.demotes@gmail.com>
2025-03-24 11:36:06 -0400

    refactor(hw2): update run.mjs to improve logging directory management and add support for multiple training runs

--- hw2/run.mjs
+++ hw2/run.mjs
@@ -13,32 +13,34 @@ console.log(`Current working directory is ${process.cwd()}`);
 
 async function run(
     params,
+    logDir,
     skipExisting = true,
     saveStates = false,
     randomStates = RandomStates,
-    logDir = getLogDir(params)
 ) {
+
+    logPath = `${logBaseDir}/${logDir}/${getLogFolder(params)}`;
 
     for (const randomState of randomStates) {
 
-        const logDirWithSeed = `${logDir}/seed=${randomState}`;
-        if (existsSync(logDirWithSeed)) {
+        const logPathWithSeed = `${logPath}/seed=${randomState}`;
+        if (existsSync(logPathWithSeed)) {
             if (skipExisting) {
-                console.log(chalk.yellow(`Skipping ${logDirWithSeed} because it already exists`));
+                console.log(chalk.yellow(`Skipping ${logPathWithSeed} because it already exists`));
                 continue;
             } else {
-                console.log(chalk.blue(`Removing ${logDirWithSeed} because it already exists`));
-                rmSync(logDirWithSeed, { recursive: true, force: true });
+                console.log(chalk.blue(`Removing ${logPathWithSeed} because it already exists`));
+                rmSync(logPathWithSeed, { recursive: true, force: true });
             }
         } else {
-            mkdirSync(logDirWithSeed, { recursive: true });
+            mkdirSync(logPathWithSeed, { recursive: true });
         }
 
         // Add the arguments to the command
         const args = [];
         Object.entries(params).forEach(([key, value]) => args.push(`--${key}`, value));
         args.push('--seed', randomState);
-        args.push('--log_dir', logDirWithSeed);
+        args.push('--log_dir', logPathWithSeed);
         if (!saveStates) {
             args.push('--save_model_step', '0');
         }
@@ -47,11 +49,11 @@ async function run(
     }
 }
 
-function getLogDir(params) {
+function getLogFolder(params) {
     const argsStr = Object.entries(params)
         .map(([key, value]) => `${key}=${value}`)
         .join('-');
-    return `${logBaseDir}/${argsStr}`;
+    return argsStr;
 }
 
 // ---------------------------------------------------------------------------
@@ -62,14 +64,32 @@ async function question1() {
         model: 'gpt',
         optimizer: 'adamw',
         n_steps: 10000,
-    }, false);
+    }, 'q1');
 
     await run({
         model: 'lstm',
         optimizer: 'adamw',
         n_steps: 10000,
-    }, false);
+    }, 'q1');
+}
+
+async function question2() {
+    r_trains = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9];
+    for (const r_train of r_trains) {
+        await run({
+            model: 'gpt',
+            optimizer: 'adamw',
+            n_steps: 10000,
+            r_train: r_train,
+        }, `q2`);
+        await run({
+            model: 'lstm',
+            optimizer: 'adamw',
+            n_steps: 10000,
+            r_train: r_train,
+        }, `q2`);
+    }
 }
 
 await question1();
-
+await question2();
