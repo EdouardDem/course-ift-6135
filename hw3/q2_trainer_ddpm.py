fix(hw3): correct mean and variance calculations in q_xt_x0 method; implement p_sample method in Trainer class

hw3/q2_trainer_ddpm.py
@@ -130,7 +130,8 @@ class Trainer:
             for t_ in tqdm(range(n_steps)):
                 
                 # TODO: Sample x_t 
-                raise NotImplementedError
+                t = torch.tensor([n_steps - t_], device=self.args.device).long()
+                x = self.diffusion.p_sample(x, t, set_seed)
             
                 if self.args.nb_save is not None and t_ in saving_steps:
                     print(f"Showing/saving samples from epoch {self.current_epoch}")
@@ -195,8 +196,10 @@ class Trainer:
         for step in tqdm(range(1, n_steps+1, 1)):
             # TODO: Generate intermediate steps
             # Hint: if GPU crashes, it might be because you accumulate unused gradient ... don't forget to remove gradient
-            raise NotImplementedError
-        
+            t = torch.tensor([n_steps - step], device=args.device).long()
+            with torch.no_grad():
+                x = self.diffusion.p_sample(x, t)
+            
             # Store intermediate result if it's a step we want to display
             if step in steps_to_show:
                 images.append(x.detach().cpu().numpy())