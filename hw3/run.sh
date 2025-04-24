db43ec0fc4bc616a8f7b4825b218b545f6545e29
Edouard Demotes <e.demotes@gmail.com>
2025-04-24 12:26:00 -0400

    fix(hw3): clean up image directories before training Q2 and Q3 models in run.sh

--- hw3/run.sh
+++ hw3/run.sh
@@ -1,5 +1,8 @@
 #! /bin/bash
 
+# Remove saved models
+rm *.pkl
+
 # Run the training script
 python q1_train_vae.py --epochs 20 --seed 42
 
@@ -11,6 +14,8 @@ python q1_generate.py
 python q1_interpolate.py
 python q1_disentangled.py
 
+rm -rf images/q2 && mkdir -p images/q2
 python q2_train.py --epochs 20 
 
+rm -rf images/q3 && mkdir -p images/q3
 python q3_train.py --epochs 20 
